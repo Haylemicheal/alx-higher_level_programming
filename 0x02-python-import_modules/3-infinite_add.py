df17e49122d6aa7f9f3a848797c9b97e51a6e4b8
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-06-02 14:05:22 +0300

    Fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argv = sys.argv
-sum = 0
+    argv = sys.argv
+    sum = 0
 
-for i in range(1, len(argv)):
-    sum = sum + int(argv[i])
-print("{}".format(sum))
+    for i in range(1, len(argv)):
+        sum = sum + int(argv[i])
+    print("{}".format(sum))
