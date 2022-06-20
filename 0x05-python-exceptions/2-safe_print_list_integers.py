45cf554991902e2591e9162d8d389c3cdfab652e
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-06-20 21:20:49 +0300

    Rename the function

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print('{:d}'.format(my_list[i]), end='')
             c = c + 1
+	except IndexError:
+		break
         except Exception:
-            continue
+            pass
     print('')
     return c
