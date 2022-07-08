bea9c0cd9ff9370335b2c7da024a46f89bc8b4db
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-07-08 20:47:53 +0300

    Create a models module and add Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A module that contains Base class
+"""
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id = None):
+        """Constructor for base class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
