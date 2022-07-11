7534eaf601fafa1b922a0b19d7f4e3b50271801d
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-07-11 20:04:55 +0300

    Fix typo

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,8 +76,9 @@ class Rectangle(Base):
 
     def display(self):
         """Dispaly the rectangle"""
+        print('\n' * self.__y, end="")
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(self.__x * " " + "#" * self.__width)
 
     def __str__(self):
         """string representation"""
